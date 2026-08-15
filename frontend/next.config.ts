8ad4d33eeec9f08401d9ff47c2afa0404ee5e902
Дарья Аветисян <daraavetisan@MacBook-Air-Dara.local>
2026-08-15 12:24:21 +0700

    Remove output:export to fix 404 on Netlify

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  // ← РЕЖИМ ЭКСПОРТА
+
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
 };
 
-export default nextConfig;
